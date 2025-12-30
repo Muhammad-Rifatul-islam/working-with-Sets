cea36717b6a82391ea669331a876890b01f92239
Muhammad Rifatul Islam <muhammadrifat707@gmail.com>
2025-12-30 15:03:07 +0600

    submit assignment

---
+++ bin/assignment1.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  Set<int> setA = {1, 2, 3, 4, 5};
+
+
+  Set<int> setB = {4, 5, 6, 7, 8};
+
+
+  print('Elements of Set A:');
+  for (var element in setA) {
+    print(element);
+  }
+
+
+  print('\nElements of Set B:');
+  for (var element in setB) {
+    print(element);
+  }
+}
